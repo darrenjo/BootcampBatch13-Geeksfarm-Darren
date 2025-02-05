15d63db9ad7f6577bb64af35e96be5789725b919
Jo Darren Adriel <darren12@rocketmail.com>
2025-02-05 09:11:34 +0700

    fixed variable name

--- tugas.js
+++ tugas.js
@@ -3,12 +3,12 @@ import { stdin as input, stdout as output } from "node:process";
 
 const rl = readline.createInterface({ input, output });
 
-const answer1 = await rl.question("What is your name? ");
-const answer2 = await rl.question("What is your phone number? ");
-const answer3 = await rl.question("What is your email? ");
+const name = await rl.question("What is your name? ");
+const phone = await rl.question("What is your phone number? ");
+const email = await rl.question("What is your email? ");
 
-console.log(`Your name: ${answer1}`);
-console.log(`Your phone: ${answer2}`);
-console.log(`Your email: ${answer3}`);
+console.log(`Your name: ${name}`);
+console.log(`Your phone: ${phone}`);
+console.log(`Your email: ${email}`);
 
 rl.close();
